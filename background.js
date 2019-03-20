e4adfefa54a15a1d0ecde015c1f4c239a8ef43a6
Justice Ogbonna <justiceo@google.com>
2019-03-19 21:25:53 -0400

    implement click to toggle

--- background.js
+++ background.js
@@ -1,5 +1,19 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ color: '#3aa757' }, function () {
-        console.log('The color is green.');
+'use strict';
+
+function toggleIcon() {
+    chrome.storage.sync.get('isEnabled', function (data) {
+        chrome.storage.sync.set({ isEnabled: !data.isEnabled }, function () {
+            console.log('Enabled is ' + !data.isEnabled);
+        });
+        chrome.browserAction.setIcon({ path: data.isEnabled ? 'disabled.png' : 'enabled.png' });
     });
+};
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({ isEnabled: false }, function () {
+        console.log('Enabled is false');
+    });
+    chrome.browserAction.setIcon({ path: 'disabled.png' });
 });
+
+chrome.browserAction.onClicked.addListener(toggleIcon);
