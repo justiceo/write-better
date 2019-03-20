206ffbe7cdd1301f188bc08d1f2704f17ca60735
Justice Ogbonna <ebesco@yahoo.com>
2019-03-19 22:40:58 -0400

    make page action remember state for url

--- background.js
+++ background.js
@@ -1,19 +1,37 @@
 'use strict';
 
-function toggleIcon() {
-    chrome.storage.sync.get('isEnabled', function (data) {
-        chrome.storage.sync.set({ isEnabled: !data.isEnabled }, function () {
-            console.log('Enabled is ' + !data.isEnabled);
+function toggleIcon(tab) {
+    const url = (new URL(tab.url)).hostname
+    chrome.storage.sync.get(url, (data) => {
+        let save = {}
+        save[url] = !data[url]
+        chrome.storage.sync.set(save, () => {
+            console.log('updated', url, 'to', save);
         });
-        chrome.browserAction.setIcon({ path: data.isEnabled ? 'disabled.png' : 'enabled.png' });
+        chrome.browserAction.setIcon({ path: data[url] ? 'disabled.png' : 'enabled.png' });
     });
 };
 
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ isEnabled: false }, function () {
-        console.log('Enabled is false');
+function checkStatus(tab) {
+    if (!tab) {
+        return
+    }
+    const url = (new URL(tab.url)).hostname
+    chrome.storage.sync.get(url, (data) => {
+        console.log('value of', url, ':', data)
+        chrome.browserAction.setIcon({ path: data[url] ? 'enabled.png' : 'disabled.png' });
     });
-    chrome.browserAction.setIcon({ path: 'disabled.png' });
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.tabs.getCurrent(checkStatus);
 });
 
+// Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(toggleIcon);
+
+// Called when user switches tab. Check if enabled for this tab
+chrome.tabs.onActivated.addListener(function (active) {
+    console.log('tab', tab.tabId, 'activated');
+    chrome.tabs.get(active.tabId, checkStatus)
+});
