388139d0936e9b9e1099504b93803ad4a2762824
Justice Ogbonna <ebesco@yahoo.com>
2019-03-22 09:39:15 -0700

    create a map of text to corrections

--- content-script/bin/content-script.js
+++ content-script/bin/content-script.js
@@ -4,20 +4,22 @@ const writeGood = require('write-good');
 chrome.runtime.onMessage.addListener(function (msg, sender, callback) {
     console.log('runtime.onMessage fired', msg);
     if (msg.text === 'analyze_doc') {
-        let p = document.getElementsByClassName('kix-page');
-        for (let i = 0; i < p.length; i++) {
-            let t = textNodes(p[i]);
-            for (let j = 0; j < t.length; j++) {
-                if (t[j].textContent.trim() != "") {
-                    console.debug('writeGood: ', t[j].textContent, '->', writeGood(t[j].textContent));
-                }
+        let allPages = document.querySelector('.kix-paginateddocumentplugin');
+        let txts = textNodes(allPages);
+        let nodeMap = {};
+        for (let i = 0; i < txts.length; i++) {
+            const txt = txts[i];
+            if (txt.textContent.trim() != "") {
+                nodeMap[txt.textContent] = writeGood(txt.textContent);
             }
         }
-        callback(document.body.innerHTML);
+        console.debug('writeGood: ', nodeMap);
+        callback('success');
     }
 });
 
 function textNodes(node) {
+    if (!node) return [];
     let all = [];
     for (node = node.firstChild; node; node = node.nextSibling) {
         if (node.nodeType == 3) all.push(node);
