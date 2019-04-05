dbe01cd4b5d5e2d4ed90f89e0cff5a24f4ccff91
Justice Ogbonna <ebesco@yahoo.com>
2019-04-05 14:38:13 -0400

    remove browser action and corresponding background js code
     - switching to page Action for simpler enabling/disabling
     - may need toggle later for changing to error icon

--- src/background.ts
+++ src/background.ts
@@ -1,44 +1,4 @@
-import { EnableOnDocs, Message, GetExtensionFile } from './shared';
-
-function toggleIcon(tab: chrome.tabs.Tab) {
-    if (!tab) {
-        return console.error('toggleIcon: tab input cannot be falsy');
-    }
-    const host = (new URL(tab.url)).hostname;
-    if (!host) {
-        return console.error('toggleIcon: hostname must be a non-nil string');
-    }
-
-    chrome.storage.sync.get(host, (data) => {
-        let save: any = {}
-        save[host] = !data[host]
-        chrome.storage.sync.set(save, () => {
-            console.log('toggleIcon: updated', host, 'to', save);
-            setTabState(tab);
-        });
-    });
-};
-
-function setTabState(tab: chrome.tabs.Tab) {
-    if (!tab) {
-        return console.error('setTabState: tab input cannot be falsy');
-    }
-    const host = (new URL(tab.url)).hostname;
-    if (!host) {
-        return console.error('setTabState: hostname must be a non-nil string');
-    }
-    chrome.storage.sync.get(host, (data) => {
-        console.log('setTabState: state of', host, 'is', data[host])
-        chrome.browserAction.setIcon({ path: data[host] ? 'images/quill-orange32.png' : 'images/quill-gray32.png' });
-        if (data[host]) {
-            chrome.tabs.sendMessage(tab.id, { type: 'analyze_doc' } as Message, () => {
-                console.log('Done analyzing doc');
-            });
-        } else {
-            chrome.tabs.sendMessage(tab.id, { type: 'cleanup' } as Message);
-        }
-    });
-}
+import { EnableOnDocs, GetExtensionFile } from './shared';
 
 chrome.runtime.onInstalled.addListener((details: chrome.runtime.InstalledDetails) => {
     console.log('runtime.onInstalled fired:' + details.reason);
@@ -46,18 +6,6 @@ chrome.runtime.onInstalled.addListener((details: chrome.runtime.InstalledDetails
     GetExtensionFile('underline.css', () => console.log('saved underline.css file to storage'));
     GetExtensionFile('hover.css', () => console.log('saved hover.css file to storage'));
     EnableOnDocs(() => console.log('enabled extensions on gdocs.'));
-});
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener((tab: chrome.tabs.Tab) => {
-    console.log('browserAction.onClicked fired: ', tab);
-    toggleIcon(tab);
-});
-
-// Called when user switches tab. Check if enabled for this tab
-chrome.tabs.onActivated.addListener((active: chrome.tabs.TabActiveInfo) => {
-    console.log('tabs.onActivated fired: ', active);
-    chrome.tabs.get(active.tabId, setTabState);
 });
 
 chrome.runtime.onSuspend.addListener(() => {
