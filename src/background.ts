23724c3103fa19b703536e090d83386024cf03e2
Justice Ogbonna <ebesco@yahoo.com>
2019-04-05 22:28:28 -0400

    removed now unused css files

--- src/background.ts
+++ src/background.ts
@@ -2,9 +2,7 @@ import { EnableOnDocs, GetExtensionFile } from './shared';
 
 chrome.runtime.onInstalled.addListener((details: chrome.runtime.InstalledDetails) => {
     console.log('runtime.onInstalled fired:' + details.reason);
-    GetExtensionFile('template.css', () => console.log('saved template.css file to storage'));
     GetExtensionFile('underline.css', () => console.log('saved underline.css file to storage'));
-    GetExtensionFile('hover.css', () => console.log('saved hover.css file to storage'));
     EnableOnDocs(() => console.log('enabled extensions on gdocs.'));
 });
 
