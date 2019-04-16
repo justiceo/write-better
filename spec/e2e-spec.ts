fa679c40eebc40bcab942b5cb1bd30590e42e989
Justice Ogbonna <ebesco@yahoo.com>
2019-04-15 20:50:26 -0400

    remove unnecessary test cases

--- spec/e2e-spec.ts
+++ spec/e2e-spec.ts
@@ -16,51 +16,12 @@ describe('browser with extension', () => {
     await browser.close();
   });
 
-  describe('smoke test', () => {
-    it('puppeteer\'s example with extension', async () => {
-      const page = await browser.newPage();
-      await page.goto('https://developers.google.com/web/');
-
-      // Type into search box.
-      await page.type('#searchbox input', 'Headless Chrome');
-
-      // Wait for suggest overlay to appear and click "show all results".
-      const allResultsSelector = '.devsite-suggest-all-results';
-      // await page.waitForSelector(allResultsSelector)
-      expectAsync(page.waitForSelector(allResultsSelector)).toBeResolved();
-      await page.click(allResultsSelector);
-
-      // Wait for the results page to load and display the results.
-      const resultsSelector = '.gsc-results .gsc-thumbnail-inside a.gs-title';
-      await page.waitForSelector(resultsSelector);
-
-      // Extract the results from the page.
-      const links = await page.evaluate(resultsSelector => {
-        const anchors = Array.from(document.querySelectorAll(resultsSelector));
-        return anchors.map(anchor => {
-          const title = anchor.textContent.split('|')[0].trim();
-          return `${title} - ${anchor.href}`;
-        });
-      }, resultsSelector);
-      console.log(links.join('\n'));
-
-      await page.close();
-    });
-  });
-
   describe('writebetter test doc', () => {
-    it('should have writebetter test doc in title', async () => {
-      const page = await browser.newPage();
-      await page.goto('https://docs.google.com/document/d/1KX_6FahTxjSIDIh07LdfIt4dfik0_S1OTzNvQsD3YOc');
-
-      expect(await page.title()).toBe('WriteBetter Test Doc - Google Docs');
-      await page.close();
-    });
-
     it('writebetter-highlights', async () => {
       const page = await browser.newPage();
       await page.goto('https://docs.google.com/document/d/1KX_6FahTxjSIDIh07LdfIt4dfik0_S1OTzNvQsD3YOc');
 
+      expect(await page.title()).toBe('WriteBetter Test Doc - Google Docs');
       await page.waitFor(3000); // TODO: figure out why last element takes a second to display.
 
       const highlights: string[] = await page.evaluate(() => {
