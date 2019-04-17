cdd405072b1d254e091723246bda7ade192ab248
Justice Ogbonna <ebesco@yahoo.com>
2019-04-17 14:17:41 -0400

    open how-to doc on post-install

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,10 @@
 chrome.runtime.onInstalled.addListener((details: chrome.runtime.InstalledDetails) => {
     console.log('runtime.onInstalled fired:' + details.reason);
+    chrome.tabs.create({
+        url: 'https://docs.google.com/document/d/1pobtU3ZX0eJkMGXBa0dcH8LkJB3jRFt31dZwY3ozeLM',
+        active: true
+    });
+    return false;
 });
 
 chrome.runtime.onSuspend.addListener(() => {
